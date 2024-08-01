8a2f2840e30c68959b79e22400f61b38ae1233e5
Money <manhtien310701@gmail.com>
2024-08-01 10:22:23 +0800

    add validation-board

--- src/validations/board.validation.js
+++ src/validations/board.validation.js
@@ -1,3 +1,15 @@
+import { StatusCodes } from 'http-status-codes'
 import Joi from 'joi'
 
+const schemaBoard = Joi.object({
+  title: Joi.string().required().min(3).max(50),
+  description: Joi.string().required().min(3).max(256)
+})
 
+const createBoard = (req, res, next) => {
+  const { title, description } = req.body
+  if (!title || !description) {
+    return res.status(400).json({ error: 'Title and description are required' })
+  }
+  res.status(StatusCodes.CREATED).json({ message: 'Board created', title, description })
+}
